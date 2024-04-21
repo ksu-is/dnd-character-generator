8408b75c7c355c7324db7073d8ff718628d37281
masonnix20 <156404951+masonnix20@users.noreply.github.com>
2024-04-21 13:34:46 -0400

    Base Stats update
    initialized core stats and derived stats

--- BaseStats.py
+++ BaseStats.py
@@ -3,4 +3,22 @@ DnD Character Generator
 Author: Mason Nix
 This section defines the core character statistics
 """
+#initializing core stats
+Str = 0
+Dex = 0
+Con = 0
+Int = 0
+Wis = 0
+Cha = 0
+race  = ""
+characterClass =""
+
+#init derived stats
+str_mod = 0
+dex_mod = 0
+int_mod = 0
+wis_mod = 0
+cha_mod = 0
+ac = 0
+
 

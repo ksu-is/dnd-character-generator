a2dd3c9eecb349ca188b08dcbd995edc301f36dc
masonnix20 <156404951+masonnix20@users.noreply.github.com>
2024-04-22 10:28:50 -0400

    Creating core character class functionality
    experimenting with how best to replicate the DnD character classes as python Classes

--- BaseStats.py
+++ BaseStats.py
@@ -11,7 +11,7 @@ Int = 0
 Wis = 0
 Cha = 0
 race  = ""
-characterClass =""
+characterClass = ""
 
 #init derived stats
 str_mod = 0
@@ -20,5 +20,27 @@ int_mod = 0
 wis_mod = 0
 cha_mod = 0
 ac = 0
+
+#classList = ["Figher", "Bard", "Rogue", "Wizard"]
+
+
+#decisions = input ("Please input a Character Name:")
+
+#Defining the python class to create character classes from DnD
+class CharacterClass:
+    def __init__(self, title, hd):
+        self.className = title
+        self.hd = hd
+
+fighter = CharacterClass("Fighter", 10)
+wizard = CharacterClass("Wizard", 6)
+
+print("Class: {} \nHit Dice: d{}".format(fighter.className, fighter.hd))
+print("Class: {} \nHit Dice: d{}".format(wizard.className, wizard.hd))
+
+    
+    
+
+
 
 
--- userInterface.py
+++ userInterface.py
@@ -2,6 +2,8 @@
 DnD Character Generator
 Author: Mason Nix
 This section gathers user input on ability scores, class, and race
-
+THIS SECTION IS NOW OBSOLETE
+I decided to work in the same file for ease. 
+Migrated to BaseStats.py
 """
 inp = input("Please Input a Character Name:")
